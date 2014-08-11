ab9e914b721fd37e34d6ce13ee777ddae6d2ddd8
Yusuke Nozoe <yusuke.nozoe@dena.jp>
2014-08-11 20:41:02 +0900

    inputrcはいれる

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc .percol.d)
-DOT_FILES=( .gitconfig .gitignore .vimrc .vim )
+DOT_FILES=( .gitconfig .gitignore .inputrc .vimrc .vim )
 
 for file in ${DOT_FILES[@]}
 do
@@ -15,5 +15,5 @@ done
 ## install oh-my-zsh
 #[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 #
-#[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
 

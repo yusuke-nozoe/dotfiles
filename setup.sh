20667ce342dbe78ba2dafa4af3300f94a0c5e60d
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2015-03-12 06:07:19 +0000

    update

--- setup.sh
+++ setup.sh
@@ -15,5 +15,7 @@ done
 ## install oh-my-zsh
 #[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 #
-[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
+[ ! -d ~/.vim/bundle ] 
+mkdir -p ~/.vim/bundle 
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
 
